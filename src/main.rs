d6ab8b353a2c268fbcea2526b4c9ed25f699b69c
Keita ATTICOT <sagara911@gmail.com>
2022-03-05 00:24:58 +0100

    🎉 init

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,21 @@
-#[cfg(feature = "chaincore")]
-extern crate crypto;
-extern crate merkletree;
+use merkle_tree::CBMT;
 
-use merkletree::merkle::MerkleTree;
-use merkletree::store::VecStore;
+struct MergeI32 {}
 
-#[cfg(feature = "chaincore")]
-mod example {
-    use std::fmt;
-    use std::hash::Hasher;
-    use crypto::sha3::{Sha3, Sha3Mode};
-    use crypto::digest::Digest;
-    use merkletree::hash::{Algorithm, Hashable};
-
-    pub struct ExampleAlgorithm(Sha3);
-
-    impl ExampleAlgorithm {
-        pub fn new() -> ExampleAlgorithm {
-            ExampleAlgorithm(Sha3::new(Sha3Mode::Sha3_256))
-        }
-    }
-
-    impl Default for ExampleAlgorithm {
-        fn default() -> ExampleAlgorithm {
-            ExampleAlgorithm::new()
-        }
-    }
-
-    impl Hasher for ExampleAlgorithm {
-        #[inline]
-        fn write(&mut self, msg: &[u8]) {
-            self.0.input(msg)
-        }
-
-        #[inline]
-        fn finish(&self) -> u64 {
-            unimplemented!()
-        }
-    }
-
-    impl Algorithm<[u8; 32]> for ExampleAlgorithm {
-        #[inline]
-        fn hash(&mut self) -> [u8; 32] {
-            let mut h = [0u8; 32];
-            self.0.result(&mut h);
-            h
-        }
-
-        #[inline]
-        fn reset(&mut self) {
-            self.0.reset();
-        }
+impl Merge for MergeI32 {
+    type Item = i32;
+    fn merge(left: &Self::Item, right: &Self::Item) -> Self::Item {
+        right.wrapping_sub(*left)
     }
 }
 
-fn main() {
-    use sha3::{Digest, Sha3_256};
-    #[cfg(feature = "chaincore")]
-    {
-
-
-        let t: MerkleTree<[u8; 32], ExampleAlgorithm, VecStore<_>> = MerkleTree::try_from_iter(vec![h1, h2, h3, h4].into_iter().map(Ok)).unwrap();
-        println!("{:?}", t.root());
-    }
-    let mut h1 = [0u8; 32];
-    let mut h2 = [0u8; 32];
-    let mut h3 = [0u8; 32];
-    let mut h4 = [0u8; 32];
-    h1[0] = 0x11;
-    h2[0] = 0x22;
-    h3[0] = 0x33;
-    h4[0] = 0x44;
-    let t: MerkleTree<[u8; 32], Sha3_256, VecStore<_>> = MerkleTree::try_from_iter(vec![h1, h2, h3, h4].into_iter().map(Ok)).unwrap();
-    println!("{:?}", t.root());
-}
+type CBMTI32 = CBMT<i32, MergeI32>;
+ fn main(){
+let leaves = vec![2i32, 3, 5, 7, 11];
+let indices = vec![0, 4];
+let proof_leaves = vec![2i32, 11];
+let root = CBMTI32::build_merkle_root(&leaves);
+let proof = CBMTI32::build_merkle_proof(&leaves, &indices).unwrap();
+let tree = CBMTI32::build_merkle_tree(leaves);
+proof.verify(&proof_leaves, &root);
+ }
